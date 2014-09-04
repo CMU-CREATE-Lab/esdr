cb66749ac378a836a70024742084b1e8b9797c94
Chris Bartley <bartley@cmu.edu>
2014-09-04 13:55:47 -0400

    using http-status module for HTTP status codes

--- middleware/error_handlers.js
+++ middleware/error_handlers.js
@@ -1,23 +1,24 @@
+var httpStatus = require('http-status');
 var log = require('log4js').getLogger();
 
 module.exports = {
    http404 : function(req, res, next) {
       log.error("In 404 error handler...");
-      var statusCode = 404;
+      var statusCode = httpStatus.NOT_FOUND;
       res.jsendClientError("Resource not found", null, statusCode);
    },
 
    development : function(err, req, res, next) {
       log.debug("In DEV error handler!");
       log.error(err);
-      var statusCode = err.status || 500;
+      var statusCode = err.status || httpStatus.INTERNAL_SERVER_ERROR;
       return res.jsendServerError(err.message, null, statusCode);
    },
 
    production : function(err, req, res, next) {
       log.debug("In PROD error handler!");
       log.error(err);
-      var statusCode = err.status || 500;
+      var statusCode = err.status || httpStatus.INTERNAL_SERVER_ERROR;
       return res.jsendServerError("Sorry, an unexpected error occurred", null, statusCode);
    }
 };
