f579b9eeed6e6e79c9dd6a24853c785a34951ee5
Chris Bartley <bartley@cmu.edu>
2014-10-14 15:21:33 -0400

    Now using my jsend-utils module

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var datastore = new BodyTrackDatastore({
                                        });
 
 // decorate express.response with JSend methods
-require('./lib/jsend');
+require('jsend-utils').decorateExpressResponse(require('express').response);
 
 var gracefulExit = function() {
    // TODO: any way (or need?) to gracefully shut down the database pool?
