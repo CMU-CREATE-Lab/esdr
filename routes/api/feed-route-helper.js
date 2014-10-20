024f121e9c75afe13513308f55e14d201b57e8a9
Chris Bartley <bartley@cmu.edu>
2014-10-20 15:03:43 -0400

    Simplified feed-route-helper's importData method, now requiring feed to be non-null

--- routes/api/feed-route-helper.js
+++ routes/api/feed-route-helper.js
@@ -4,31 +4,26 @@ var log = require('log4js').getLogger();
 module.exports = function(FeedModel) {
 
    this.importData = function(res, feed, data) {
-      if (feed) {
-         if (data) {
-            FeedModel.importData(feed,
-                                 data,
-                                 function(err, importResult) {
-                                    if (err) {
-                                       // See if the error contains a JSend data object.  If so, pass it on through.
-                                       if (typeof err.data !== 'undefined' &&
-                                           typeof err.data.code !== 'undefined' &&
-                                           typeof err.data.status !== 'undefined') {
-                                          return res.jsendPassThrough(err.data);
-                                       }
-                                       return res.jsendServerError("Failed to import data: " + err.message, null);
+      if (data) {
+         FeedModel.importData(feed,
+                              data,
+                              function(err, importResult) {
+                                 if (err) {
+                                    // See if the error contains a JSend data object.  If so, pass it on through.
+                                    if (typeof err.data !== 'undefined' &&
+                                        typeof err.data.code !== 'undefined' &&
+                                        typeof err.data.status !== 'undefined') {
+                                       return res.jsendPassThrough(err.data);
                                     }
-
-                                    return res.jsendSuccess(importResult, httpStatus.OK); // HTTP 200 OK
+                                    return res.jsendServerError("Failed to import data: " + err.message, null);
                                  }
-            );
-         }
-         else {
-            return res.jsendClientError("No data received", null, httpStatus.BAD_REQUEST);
-         }
+
+                                 return res.jsendSuccess(importResult, httpStatus.OK); // HTTP 200 OK
+                              }
+         );
       }
       else {
-         return res.jsendClientError("Unknown or invalid feed", null, httpStatus.NOT_FOUND);
+         return res.jsendClientError("No data received", null, httpStatus.BAD_REQUEST);
       }
    };
 
