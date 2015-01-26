589d4a2a63ad3f8d66b9901c728209d1362b2eff
Chris Bartley <bartley@cmu.edu>
2015-01-26 13:55:21 -0500

    Added stubs for Clients/Products/Devices/Feeds pages

--- routes/home/index.js
+++ routes/home/index.js
@@ -5,4 +5,20 @@ router.get('/', function(req, res) {
    res.render('home/index', { title : "ESDR"});
 });
 
+router.get('/clients', function(req, res) {
+   res.render('home/clients', { title : "ESDR: Clients", entity: "clients"});
+});
+
+router.get('/products', function(req, res) {
+   res.render('home/products', { title : "ESDR: Products", entity: "products"});
+});
+
+router.get('/devices', function(req, res) {
+   res.render('home/devices', { title : "ESDR: Devices", entity: "devices"});
+});
+
+router.get('/feeds', function(req, res) {
+   res.render('home/feeds', { title : "ESDR: Feeds", entity: "feeds"});
+});
+
 module.exports = router;
