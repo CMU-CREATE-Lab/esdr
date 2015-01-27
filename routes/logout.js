dac5c90292bb41201100e3c7a8bf6a2c4659d81d
Chris Bartley <bartley@cmu.edu>
2015-01-27 14:20:43 -0500

    No longer destroying tokens on logout
    Destroying tokens should really only be done once the last session for that user is destroyed. Use case: User logs into the site using two different browsers.  They'll both get the same access token, but if she logs out on one browser, the other browser will still need the oauth tokens.  Revisit this later.

--- routes/logout.js
+++ routes/logout.js
@@ -8,19 +8,23 @@ module.exports = function(TokenModel) {
    router.get('/', function(req, res) {
       if (req.user) {
          var userId = req.user.id;
-         process.nextTick(function() {
-            log.debug("Destroying tokens for user [" + userId + "]");
-            TokenModel.remove(userId, config.get("esdrClient:id"), function(err, wasSuccessful) {
-               if (err) {
-                  log.error("Error while trying to destroy tokens for user [" + userId + "]: " + err);
-               }
-               else {
-                  log.debug("Tokens destroyed for user [" + userId + "]: " + wasSuccessful);
-               }
-            });
-         });
+         log.debug("Destroying session for user [" + userId + "]");
+
+         // TODO: Destroying tokens should really only be done once the last session for that user is destroyed.
+         // Use case: User logs into the site using two different browsers.  They'll both get the same access token,
+         // but if she logs out on one browser, the other browser will still need the oauth tokens.  Revisit this later.
+         //process.nextTick(function() {
+         //   log.debug("Destroying tokens for user [" + userId + "]");
+         //   TokenModel.remove(userId, config.get("esdrClient:id"), function(err, wasSuccessful) {
+         //      if (err) {
+         //         log.error("Error while trying to destroy tokens for user [" + userId + "]: " + err);
+         //      }
+         //      else {
+         //         log.debug("Tokens destroyed for user [" + userId + "]: " + wasSuccessful);
+         //      }
+         //   });
+         //});
       }
-      log.debug("Destroying session for user [" + userId + "]");
       req.session = null;
       req.logout();
       res.redirect('/');
