2b915a05778ac098e89ee713598cd15426b53117
Chris Bartley <bartley@cmu.edu>
2015-01-27 13:05:47 -0500

    Initial checkin of page to create and view clients

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,24 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../../config');
 
 router.get('/', function(req, res) {
-   res.render('home/index', { title : "ESDR"});
+   res.render('home/index', { title : "ESDR" });
 });
 
 router.get('/clients', function(req, res) {
-   res.render('home/clients', { title : "ESDR: Clients", entity: "clients"});
+   res.render('home/clients',
+              {
+                 title : "ESDR: Clients",
+                 entity : "clients",
+                 defaultResetPasswordUrl : config.get("esdrClient:resetPasswordUrl"),
+                 defaultVerificationUrl : config.get("esdrClient:verificationUrl")
+              });
 });
 
 router.get('/products', function(req, res) {
-   res.render('home/products', { title : "ESDR: Products", entity: "products"});
+   res.render('home/products', { title : "ESDR: Products", entity : "products" });
 });
 
 router.get('/devices', function(req, res) {
-   res.render('home/devices', { title : "ESDR: Devices", entity: "devices"});
+   res.render('home/devices', { title : "ESDR: Devices", entity : "devices" });
 });
 
 router.get('/feeds', function(req, res) {
-   res.render('home/feeds', { title : "ESDR: Feeds", entity: "feeds"});
+   res.render('home/feeds', { title : "ESDR: Feeds", entity : "feeds" });
 });
 
 module.exports = router;
