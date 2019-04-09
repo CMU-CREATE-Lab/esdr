1b4086a1fce8d60caa817c793b2eb597620a11eb
Chris Bartley <bartley@cmu.edu>
2019-04-09 16:25:19 -0400

    var --> const, updated usage of bcrypt to use promises

--- models/Clients.js
+++ models/Clients.js
@@ -1,38 +1,39 @@
-var trimAndCopyPropertyIfNonEmpty = require('../lib/objectUtils').trimAndCopyPropertyIfNonEmpty;
-var bcrypt = require('bcrypt');
-var JaySchema = require('jayschema');
-var jsonValidator = new JaySchema();
-var ValidationError = require('../lib/errors').ValidationError;
-var Query2Query = require('query2query');
-var config = require('../config');
-var log = require('log4js').getLogger('esdr:models:clients');
+const trimAndCopyPropertyIfNonEmpty = require('../lib/objectUtils').trimAndCopyPropertyIfNonEmpty;
+const bcrypt = require('bcrypt');
+const JaySchema = require('jayschema');
+const jsonValidator = new JaySchema();
+const ValidationError = require('../lib/errors').ValidationError;
+const Query2Query = require('query2query');
+const config = require('../config');
+const log = require('log4js').getLogger('esdr:models:clients');
 
-var CREATE_TABLE_QUERY = " CREATE TABLE IF NOT EXISTS `Clients` ( " +
-                         "`id` bigint(20) NOT NULL AUTO_INCREMENT, " +
-                         "`displayName` varchar(255) NOT NULL, " +
-                         "`clientName` varchar(255) NOT NULL, " +
-                         "`clientSecret` varchar(255) NOT NULL, " +
-                         "`email` varchar(255) NOT NULL, " +
-                         "`verificationUrl` varchar(512) NOT NULL, " +
-                         "`resetPasswordUrl` varchar(512) NOT NULL, " +
-                         "`creatorUserId` bigint(20) DEFAULT NULL, " +
-                         "`isPublic` boolean DEFAULT 0, " +
-                         "`created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
-                         "`modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
-                         "PRIMARY KEY (`id`), " +
-                         "KEY `displayName` (`displayName`), " +
-                         "UNIQUE KEY `unique_clientName` (`clientName`), " +
-                         "KEY `email` (`email`), " +
-                         "KEY `creatorUserId` (`creatorUserId`), " +
-                         "KEY `isPublic` (`isPublic`), " +
-                         "KEY `created` (`created`), " +
-                         "KEY `modified` (`modified`), " +
-                         "CONSTRAINT `clients_creatorUserId_fk_1` FOREIGN KEY (`creatorUserId`) REFERENCES `Users` (`id`) " +
-                         ") ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8";
+// noinspection SqlNoDataSourceInspection
+const CREATE_TABLE_QUERY = " CREATE TABLE IF NOT EXISTS `Clients` ( " +
+                           "`id` bigint(20) NOT NULL AUTO_INCREMENT, " +
+                           "`displayName` varchar(255) NOT NULL, " +
+                           "`clientName` varchar(255) NOT NULL, " +
+                           "`clientSecret` varchar(255) NOT NULL, " +
+                           "`email` varchar(255) NOT NULL, " +
+                           "`verificationUrl` varchar(512) NOT NULL, " +
+                           "`resetPasswordUrl` varchar(512) NOT NULL, " +
+                           "`creatorUserId` bigint(20) DEFAULT NULL, " +
+                           "`isPublic` boolean DEFAULT 0, " +
+                           "`created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+                           "`modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
+                           "PRIMARY KEY (`id`), " +
+                           "KEY `displayName` (`displayName`), " +
+                           "UNIQUE KEY `unique_clientName` (`clientName`), " +
+                           "KEY `email` (`email`), " +
+                           "KEY `creatorUserId` (`creatorUserId`), " +
+                           "KEY `isPublic` (`isPublic`), " +
+                           "KEY `created` (`created`), " +
+                           "KEY `modified` (`modified`), " +
+                           "CONSTRAINT `clients_creatorUserId_fk_1` FOREIGN KEY (`creatorUserId`) REFERENCES `Users` (`id`) " +
+                           ") ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8";
 
-var MAX_FOUND_CLIENTS = 100;
+const MAX_FOUND_CLIENTS = 100;
 
-var query2query = new Query2Query();
+const query2query = new Query2Query();
 query2query.addField('id', true, true, false, Query2Query.types.INTEGER);
 query2query.addField('displayName', true, true, false);
 query2query.addField('clientName', true, true, false);
@@ -44,7 +45,7 @@ query2query.addField('isPublic', true, true, false, Query2Query.types.BOOLEAN);
 query2query.addField('created', true, true, false, Query2Query.types.DATETIME);
 query2query.addField('modified', true, true, false, Query2Query.types.DATETIME);
 
-var JSON_SCHEMA = {
+const JSON_SCHEMA = {
    "$schema" : "http://json-schema.org/draft-04/schema#",
    "title" : "Client",
    "description" : "An ESDR client",
@@ -106,7 +107,7 @@ module.exports = function(databaseHelper) {
 
    this.create = function(clientDetails, creatorUserId, callback) {
       // first build a copy and trim some fields
-      var client = {
+      const client = {
          creatorUserId : creatorUserId,
          clientSecret : clientDetails.clientSecret,
          // don't allow private clients if the creating user isn't auth'd
@@ -136,27 +137,25 @@ module.exports = function(databaseHelper) {
          }
 
          // if validation was successful, then hash the secret
-         bcrypt.hash(client.clientSecret, 8, function(err2, hashedSecret) {
-            if (err2) {
-               return callback(err2);
-            }
+         bcrypt.hash(client.clientSecret, 8)
+               .then(hashedSecret => {
+                  // now that we have the hashed secret, try to insert
+                  client.clientSecret = hashedSecret;
+                  // noinspection SqlNoDataSourceInspection
+                  databaseHelper.execute("INSERT INTO Clients SET ?", client, function(err2, result) {
+                     if (err2) {
+                        return callback(err2);
+                     }
 
-            // now that we have the hashed secret, try to insert
-            client.clientSecret = hashedSecret;
-            databaseHelper.execute("INSERT INTO Clients SET ?", client, function(err2, result) {
-               if (err2) {
-                  return callback(err2);
-               }
-
-               return callback(null, {
-                  insertId : result.insertId,
-                  // include these because they might have been modified by the trimming
-                  displayName : client.displayName,
-                  clientName : client.clientName
-               });
-            });
-
-         });
+                     return callback(null, {
+                        insertId : result.insertId,
+                        // include these because they might have been modified by the trimming
+                        displayName : client.displayName,
+                        clientName : client.clientName
+                     });
+                  });
+               })
+               .catch(err => callback(err));
       });
    };
 
@@ -171,7 +170,7 @@ module.exports = function(databaseHelper) {
                            // fields to the SELECT statement.  This is so we can filter out the the email,
                            // verificationUrl, and resetPasswordUrl if the client record is not public or not owned by
                            // the auth'd user.  We'll remove them from the results below if not requested by user.
-                           var restrictedSql = [
+                           const restrictedSql = [
                               "SELECT " + queryParts.selectFields.join(',') + ",isPublic,creatorUserId",
                               "FROM Clients",
                               queryParts.whereClause,
@@ -193,8 +192,8 @@ module.exports = function(databaseHelper) {
 
                               // if the user didn't explicitly select the isPublic and creatorUserId, then we'll want to
                               // strip those fields from the response
-                              var willDeleteIsPublicField = (queryParts.selectFields.indexOf('isPublic') < 0);
-                              var willDeleteCreatorUserIdField = (queryParts.selectFields.indexOf('creatorUserId') < 0);
+                              const willDeleteIsPublicField = (queryParts.selectFields.indexOf('isPublic') < 0);
+                              const willDeleteCreatorUserIdField = (queryParts.selectFields.indexOf('creatorUserId') < 0);
 
                               // For every found client, do the following:
                               // 1) if the client is private or not owned by the auth'd user, then remove the email,
@@ -202,7 +201,7 @@ module.exports = function(databaseHelper) {
                               // 2) if the user didn't ask for the isPublic field, remove it
                               // 3) if the user didn't ask for the creatorUserId field, remove it
                               result.rows.forEach(function(row) {
-                                 if (!row.isPublic && (authUserId == null || (row.creatorUserId != authUserId))) {
+                                 if (!row.isPublic && (authUserId == null || (row.creatorUserId !== authUserId))) {
                                     delete row.email;
                                     delete row.verificationUrl;
                                     delete row.resetPasswordUrl;
@@ -232,6 +231,7 @@ module.exports = function(databaseHelper) {
     * @param {function} callback function with signature <code>callback(err, client)</code>
     */
    this.findByNameAndSecret = function(clientName, clearTextSecret, callback) {
+      // noinspection SqlNoDataSourceInspection,SqlDialectInspection
       findClient("SELECT * FROM Clients WHERE clientName=?", [clientName], function(err, client) {
          if (err) {
             return callback(err);
@@ -245,7 +245,7 @@ module.exports = function(databaseHelper) {
       });
    };
 
-   var findClient = function(query, params, callback) {
+   const findClient = function(query, params, callback) {
       databaseHelper.findOne(query, params, function(err, client) {
          if (err) {
             log.error("Error trying to find client: " + err);
@@ -256,7 +256,7 @@ module.exports = function(databaseHelper) {
       });
    };
 
-   var isValidSecret = function(client, clearTextSecret) {
+   const isValidSecret = function(client, clearTextSecret) {
       return bcrypt.compareSync(clearTextSecret, client.clientSecret);
    };
 };
