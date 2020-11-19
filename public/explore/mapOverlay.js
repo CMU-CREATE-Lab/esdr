bcd3f5f8940ea3fb61d17bb620391ba74bf28799
Dömötör Gulyás <dognotdog@gmail.com>
2020-11-18 23:02:50 -0500

    tweaks to colorscales and map colors

--- public/explore/mapOverlay.js
+++ public/explore/mapOverlay.js
@@ -1264,19 +1264,19 @@ _binarySearch(array, predicate) {
 
 	sparklineColorMap(feedId, channelName) {
 		if (channelName.indexOf("tVOC") == 0) {
-			return {texture: "colorscale-rainbow.png", range: {min: 0.0, max: 2000.0}}
+			return {texture: "colorscale-tVOC_0_7000_ppb.png", range: {min: 0.0, max: 7000.0}}
 		}
 		else if (channelName.indexOf("PM") == 0) {
 			return {texture: "colorscale-PM25_0_300_ug.png", range: {min: 0.0, max: 300.0}}
 		}
 		else if (channelName.indexOf("SO2_PPM") == 0) {
-			return {texture: "colorscale-rainbow.png", range: {min: 0.0, max: 13.0}}
+			return {texture: "colorscale-SO2_0_804_ppb.png", range: {min: 0.0, max: 0.804}}
 		}
 		else if (channelName.indexOf("SO2_PPB") == 0) {
-			return {texture: "colorscale-rainbow.png", range: {min: 0.0, max: 13000.0}}
+			return {texture: "colorscale-SO2_0_804_ppb.png", range: {min: 0.0, max: 804.0}}
 		}
 		else if (channelName.indexOf("SO2") == 0) {
-			return {texture: "colorscale-rainbow.png", range: {min: 0.0, max: 13.0}}
+			return {texture: "colorscale-SO2_0_804_ppb.png", range: {min: 0.0, max: 804.0}}
 		}
 		else {
 			return {texture: undefined, range: undefined}
